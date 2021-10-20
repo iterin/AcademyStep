05381c62bd0e0e6b03f99ca64d5ac73f3a502a25
iterin <iterin@me.com>
2021-10-20 20:14:31 +0500

    Ostap

---
+++ Ostap/Ostap/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ostap
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Введите целое число (1 <= n <= 50): ");
+			int n = Convert.ToInt32(Console.ReadLine());
+			int count = 0;
+			int[] array = new int[n];
+			for (int i = 1; i <= n; i++)
+			{
+				if (i % 2 != 0)
+					count++;
+			}
+			if (count % 2 == 0)
+				Console.WriteLine("Black");
+			else
+				Console.WriteLine("Grimy");
+		}
+	}
+}
