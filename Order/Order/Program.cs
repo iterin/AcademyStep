54d8ad23829c1ec4270748476224bf5077573972
iterin <iterin@me.com>
2021-12-20 18:42:35 +0500

    Order

---
+++ Order/Order/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Order
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var reader = new CustomerReader("Customers.json");
+			foreach(var c in reader.ReadAll())
+				Console.WriteLine($"{c.Id}, {c.Name}, {c.Phone}");
+		}
+	}
+}
