137b027f779399b634a8a1098d797d06a5e075ea
ivan <iterin@me.com>
2021-12-25 23:10:54 +0500

    доп изменения

---
+++ Order/Order/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Order
+{
+	public class Order
+	{
+		public Guid Customer { get; set; }
+		public Guid Product { get; set; }
+		Order() { }
+		Order(Guid customerId, Guid productId)
+		{
+			customerId = Customer;
+			productId = Product;
+		}
+	}
+}
