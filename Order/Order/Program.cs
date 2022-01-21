07083bdac11e4dfa7060b3e1d417ae051e11ac7b
ivan <iterin@me.com>
2022-01-21 08:22:00 +0500

    new

---
+++ Order/Order/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Order
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string custmonerName = "Ilya Pavlovich";
+			string productName = "Product6";
+
+			var customer = new CustomerReader("Customers.json");
+			var product = new ProductReader("Product.json");
+			var order = new OrderReader("Orders2.json");
+
+            var customers = customer.ReadAll().ToArray();
+            var products = product.ReadAll().ToArray();
+;
+            order.ReadAll(products, customers);
+
+			var SumOrders = customers.Where(c => c.Name == custmonerName).FirstOrDefault().Products.Sum(x => x.Price);
+			Console.WriteLine($"Сумма заказов {custmonerName} равна {SumOrders}");
+
+			var CountProducts = customers.Sum(c => c.Products.Count(p => p.Name == productName));		
+			Console.WriteLine($"Количество проданого { productName} равно {CountProducts}");
+
+        }
+	}
+}
